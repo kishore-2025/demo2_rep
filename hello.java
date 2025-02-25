6fd1bf9b512b72f472cb548ac3f54000b014da7a
kishore-2025 <22p61a05p3@vbithyd.ac.in>
2025-02-25 12:03:23 +0530

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,26 @@
+pipeline
+{
+agent any
+stages
+{
+stage('clone')
+{
+steps{
+git 'https://github.com/kishore-2025/demo2_rep.git'
+}
+}
+stage('build')
+{
+steps{
+sh 'javac hello.java'
+}
+}
+  stage('run')
+  {
+    steps{
+      sh 'hello java'
+    }
+    
+  }
+}
+}
